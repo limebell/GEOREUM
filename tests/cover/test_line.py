b3fdaaa3d47ac2c6515ba0c2913db2ce34d925ae
Jaejin <fovjfozk@ijaejin-ui-MacBookPro.local>
2020-06-11 00:04:44 +0900

    cover.test unittest -> pytest

--- tests/cover/test_line.py
+++ tests/cover/test_line.py
@@ -1,15 +1,9 @@
 from src.cover.cover import Line, Cover
-import unittest
 
-class TestLine(unittest.TestCase):
-    def test_line(self):
-        filepath = "/path/to/python/test.py"
-        line_no = 112
-        line_text = "def foo():\n"
-        line = Line(filepath, line_no, line_text)
-        self.assertEqual(str(line), line_text[:-1])
-        self.assertEqual(int(line), line_no)
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_line():
+    filepath = "/path/to/python/test.py"
+    line_no = 112
+    line_text = "def foo():\n"
+    line = Line(filepath, line_no, line_text)
+    assert str(line) ==  line_text[:-1]
+    assert int(line) == line_no
