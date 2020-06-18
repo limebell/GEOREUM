0b59b278a71a3733c912c8b60a562e5bf6e4ba2a
LeeKyuSung <ks5050577@gmail.com>
2020-06-18 14:28:27 +0900

    rollback test_line.py print

--- tests/cover/test_line.py
+++ tests/cover/test_line.py
@@ -7,6 +7,10 @@ def test_line():
     line_text = "def foo():\n"
     line = Line(filepath, line_no, line_text)
     line.getHash()
-    assert str(line) ==  line_text[:-1]
+    assert str(line) == line_text[:-1]
     assert int(line) == line_no
     
+    if str(line) == line_text[:-1]:
+        print("If branch. This should be printed")
+    else:
+        print("Else branch. This should not be printed")
