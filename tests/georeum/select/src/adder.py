ee2bf6b0f6dc6ef3131ae9b8a228b0fa03e6a21f
lime <lime_bell@naver.com>
2020-06-19 20:02:19 +0900

    test_georeum modified

--- tests/georeum/select/src/adder.py
+++ tests/georeum/select/src/adder.py
@@ -1,7 +1,8 @@
 def add(a: int, b: int) -> int:
     c = a + b
     if c < 100:
-        return c + 1 # This line is modified
+        c = c + 1  # This line is added
+        return c
     else:
         print("Result is too large: %d" % c)
         return -1
