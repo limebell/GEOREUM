e2a04f825303843b7758fc9a305094d932fb3633
lime <lime_bell@naver.com>
2020-06-20 21:30:31 +0900

    Add comments

--- src/georeum/georeum.py
+++ src/georeum/georeum.py
@@ -13,6 +13,12 @@ class CoverObject:
 
 class Georeum:
     def __init__(self, root_directory: str, test_directory: str, rel_cache_directory: str = ".cache"):
+        """
+        Initializes Georeum instance.
+        :param root_directory: Absolute path of root directory of project want to examine.
+        :param test_directory: Absolute path Directory that has tests. It's commonly \"root_directory\"/tests/
+        :param rel_cache_directory: Relative path of cache directory that Georeum uses. Default is \"root_directory\"/.cache
+        """
         self.root_directory = root_directory
         self.test_directory = test_directory
         self.cache_directory = os.path.join(root_directory, rel_cache_directory)
@@ -20,6 +26,11 @@ class Georeum:
 
     @staticmethod
     def search_py_file(target_directory: str) -> list:
+        """
+        Search all .py files in target directory.
+        :param target_directory: Absolute path of directory that want to examine.
+        :return: List of pathes of .py files.
+        """
         # find all of py file in target_directory recursively
         target_file_list = []
         try:
@@ -38,6 +49,9 @@ class Georeum:
         return target_file_list
 
     def save(self):
+        """
+        Saves current coverage and diff cache.
+        """
         # TODO should manage dir_path for real tool release
         # 1. hash latest update code and save
         self.manager.update_cache()
@@ -55,9 +69,17 @@ class Georeum:
                     open(os.path.join(self.cache_directory, "coverage.bin"), 'wb'))
 
     def __contains_line(self, cover_object: CoverObject, report: DiffReport) -> bool:
+        """
+        A function that finds whether diff exists in coverage
+        :param cover_object: Coverage to examine.
+        :param report: DiffReport typed class that has diff information.
+        :return: A boolean value indicates whether diff exists in coverage
+        """
         for covered in cover_object.covered.values():
+            # relative path of covered file with respect to root directory
             rel_path = os.path.relpath(covered.file_path, self.root_directory)
             for df in report.modified_files():
+                # if coverage is in modified files:
                 if df.name == rel_path and \
                         (covered.line_no in df.added or covered.line_no in df.removed):
                     return True
@@ -65,16 +87,21 @@ class Georeum:
         return False
 
     def select_test_case(self) -> list:
+        """
+        Selects test cases that are needed to be run.
+        :return: A list of path of test cases.
+        """
         # TODO should manage dir_path for real tool release
 
         # get latest code coverage
         cover_object_list = pickle.load(open(os.path.join(self.cache_directory, "coverage.bin"), 'rb'))
 
-        # find diff of all update code & generate test cases
+        # find diff of all update code
         # TODO: this does not covers the case where modified lines are at the edge of the coverage
         diff_report = self.manager.analyze()
         selected_tests = []
         for cover_object in cover_object_list:
+            # if the coverage of a test has diff, add its path to selected_tests list
             if self.__contains_line(cover_object, diff_report):
                 selected_tests.append(cover_object.file_path)
 
